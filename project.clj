9d43efe6ccd9cd0c71c0f1a9c2da2967b162c9ac
Camilo Roca <camilo.roca@cobi.bike>
2019-09-27 14:57:06 +0200

    strint template dep added

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [instaparse "1.4.10"]])
+                 [instaparse "1.4.10"]
+                 [org.clojure/core.incubator "0.1.4"]])
