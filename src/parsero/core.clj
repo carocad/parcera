5a6b8f6dc373dc9a44353185f08b7f952bfd2a9e
Camilo Roca <camilo.roca@cobi.bike>
2019-09-29 00:12:11 +0200

    hide lexers fix: dont allow regexes with spaces between # and "

--- src/parsero/core.clj
+++ src/parsero/core.clj
@@ -10,6 +10,8 @@
 ;; / is a valid symbol as long as it is not part of the name
 (def symbol-head "(?![0-9:#])")
 
+(def string-regex "\"[^\"\\\\]*(?:\\\\.[^\"\\\\]*)*\"")
+
 (def grammar
   (strint/<<
     "file: forms
@@ -74,59 +76,59 @@
 
     lambda: '#(' form* ')';
 
-    metadata: '^' (map_metadata  | shorthand_metadata);
+    metadata: <'^'> (map_metadata  | shorthand_metadata);
 
-    map_metadata: map form
+    <map_metadata>: map form
 
-    shorthand_metadata: (symbol | STRING | keyword ) form
+    <shorthand_metadata>: (symbol | STRING | keyword ) form
 
-    regex: '#' STRING;
+    regex: #'#~{string-regex}';
 
-    var_quote: '#\\'' symbol;
+    var_quote: <'\\''> symbol;
 
-    quote: '\\'' form;
+    quote: <'\\''> form;
 
-    backtick: '`' form;
+    backtick: <'`'> form;
 
-    unquote: '~' form;
+    unquote: <'~'> form;
 
-    unquote_splicing: '~@' form;
+    unquote_splicing: <'~@'> form;
 
-    deref: '@' form;
+    deref: <'@'> form;
 
-    discard: '#_' form;
+    discard: <'#_'> form;
 
-    tag: '#' symbol form;
+    tag: <'#'> symbol form;
 
     (* Lexers -------------------------------------------------------------- *)
 
-    STRING : #'^\"[^\"\\\\]*(?:\\\\.[^\"\\\\]*)*\"';
+    STRING : #'~{string-regex}';
 
     NIL : 'nil';
 
     BOOLEAN : 'true' | 'false' ;
 
-    SIMPLE_SYMBOL: #'(~{symbol-head}~{valid-characters})|\\/';
+    <SIMPLE_SYMBOL>: #'(~{symbol-head}~{valid-characters})|\\/';
 
-    NAMESPACED_SYMBOL: #'~{symbol-head}~{valid-characters}\\/~{valid-characters}';
+    <NAMESPACED_SYMBOL>: #'~{symbol-head}~{valid-characters}\\/~{valid-characters}';
 
-    SIMPLE_KEYWORD: #':~{valid-characters}';
+    <SIMPLE_KEYWORD>: #':~{valid-characters}';
 
-    NAMESPACED_KEYWORD: #':~{valid-characters}\\/~{valid-characters}';
+    <NAMESPACED_KEYWORD>: #':~{valid-characters}\\/~{valid-characters}';
 
-    MACRO_KEYWORD: #'::~{valid-characters}';
+    <MACRO_KEYWORD>: #'::~{valid-characters}';
 
-    DOUBLE: #'([-+]?[0-9]+(\\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?'
+    <DOUBLE>: #'([-+]?[0-9]+(\\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?'
 
-    RATIO: #'([-+]?[0-9]+)/([0-9]+)'
+    <RATIO>: #'([-+]?[0-9]+)/([0-9]+)'
 
-    LONG: #'([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?'
+    <LONG>: #'([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?'
 
     COMMENT: #';.*';
 
-    UNICODE_CHAR: #'\\\\u[0-9D-Fd-f]{4}';
+    <UNICODE_CHAR>: #'\\\\u[0-9D-Fd-f]{4}';
 
-    NAMED_CHAR: #'\\\\(newline|return|space|tab|formfeed|backspace|c)';"))
+    <NAMED_CHAR>: #'\\\\(newline|return|space|tab|formfeed|backspace|c)';"))
 
 (def parser (instaparse/parser grammar :auto-whitespace :comma))
 
