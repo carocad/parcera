1fa379401484777e31848af97450e417df02dba8
Camilo Roca <camilo.roca@cobi.bike>
2019-10-03 21:43:49 +0200

    travis config added license fixed to lgpl

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject parsero "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Safe Clojure(script) parser that will make you smile"
+  :url "https://github.com/carocad/parsero"
+  :license {:name "LGPLv3"
+            :url  "https://github.com/carocad/parsero/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [instaparse "1.4.10"]
-                 [org.clojure/test.check "0.10.0"]])
+                 [org.clojure/test.check "0.10.0"]]
+  :profiles {:dev {:plugins      [[jonase/eastwood "0.2.9"]]}})
+
