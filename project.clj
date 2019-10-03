69a2d77e5395e1b8e09ac2b0b5d04c3f11253071
Camilo Roca <camilo.roca@cobi.bike>
2019-10-03 22:13:26 +0200

    deps updated symbols updated

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "LGPLv3"
             :url  "https://github.com/carocad/parsero/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [instaparse "1.4.10"]
+                 [instaparse/instaparse "1.4.10"]
                  [org.clojure/test.check "0.10.0"]]
-  :profiles {:dev {:dependencies [[criterium "0.4.4"]];; benchmark]
-                   :plugins      [[jonase/eastwood "0.2.9"]]}}
+  :profiles {:dev {:dependencies [[criterium/criterium "0.4.5"]];; benchmark]
+                   :plugins      [[jonase/eastwood "0.3.5"]]}}
   :test-selectors {:default     (fn [m] (not (some #{:benchmark} (keys m))))
                    :benchmark   :benchmark})
