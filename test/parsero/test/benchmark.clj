c5754c0be63c3ce62f523824de869a924ebadce4
Camilo Roca <camilo.roca@cobi.bike>
2019-10-03 22:19:00 +0200

    use openjdk alias ns

--- test/parsero/test/benchmark.clj
+++ test/parsero/test/benchmark.clj
@@ -1,14 +1,15 @@
 (ns parsero.test.benchmark
   (:require [clojure.test :refer [deftest is testing]]
             [clojure.test.check :as tc]
-            [criterium.core :as c]))
+            [criterium.core :as c]
+            [parsero.test.core :as pt]))
 
 (deftest ^:benchmark parsing
   (println "Benchmark: Time parsing Clojure values ⌛")
-  (c/quick-bench (tc/quick-check 100 parsero.test.core/symmetric
+  (c/quick-bench (tc/quick-check 100 pt/validity
                             :os :runtime :verbose)))
 
 (deftest ^:benchmark roundtrip
   (println "Benchmark: Round trip of Clojure values 🚀")
-  (c/quick-bench (tc/quick-check 100 parsero.test.core/symmetric
+  (c/quick-bench (tc/quick-check 100 pt/symmetric
                                  :os :runtime :verbose)))
