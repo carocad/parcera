f5d07cf7a8ef19fc19191749416641d0ded91308
Camilo Roca <camilo.roca@cobi.bike>
2019-10-05 20:18:25 +0200

    auto changelog added

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Grammar-based Clojure(script) parser"
   :url "https://github.com/carocad/parcero"
   :license {:name "LGPLv3"
-            :url  "https://github.com/carocad/parcero/blob/master/LICENSE"}
+            :url  "https://github.com/carocad/parcero/blob/master/LICENSE.md"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [instaparse/instaparse "1.4.10"]]
   :profiles {:dev {:dependencies [[criterium/criterium "0.4.5"] ;; benchmark
