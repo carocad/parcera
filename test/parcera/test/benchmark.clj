504a8c4b60115e2fea04e19c82d20bab727a8e4c
Camilo Roca <camilo.roca@cobi.bike>
2019-10-12 13:22:28 +0200

    benchmark added

--- test/parcera/test/benchmark.clj
+++ test/parcera/test/benchmark.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer [deftest is testing]]
             [clojure.test.check :as tc]
             [criterium.core :as criterium]
-            [parcera.test.core :as pt]))
+            [parcera.test.core :as pt]
+            [parcera.core :as parcera]))
 
 (deftest ^:benchmark parsing
   (println "Benchmark: Time parsing Clojure values ⌛")
@@ -14,4 +15,17 @@
   (newline)
   (println "Benchmark: Round trip of Clojure values 🚀")
   (criterium/quick-bench (tc/quick-check 30 pt/symmetric)
+                         :os :runtime :verbose))
+
+
+;; execute last ... hopefully
+(deftest ^:benchmark z-known-namespace
+  (newline)
+  (newline)
+  (println "Benchmark: Parsing parcera namespace with traces 👮")
+  (criterium/quick-bench (parcera/clojure (str '(ns parcera.core
+                                                  (:require [instaparse.core :as instaparse]
+                                                            [clojure.data :as data]
+                                                            [clojure.string :as str])))
+                                          :trace true)
                          :os :runtime :verbose))
