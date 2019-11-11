c5a3f97d694fd74dc35f6879a4722963416679e9
Camilo Roca <camilo.roca@cobi.bike>
2019-11-11 21:31:01 +0100

    fix: deactivate js to allow compilation

--- src/clojure/parcera/antlr/javascript.cljs
+++ src/clojure/parcera/antlr/javascript.cljs
@@ -5,48 +5,52 @@
   ;; am I suppose to code the whole thing and hope that it works by running
   ;; the tests 🤔 ... I can feel the pain of other languages 😭
   (:require [parcera.antlr.protocols :as antlr]
-            [antlr4.index :as runtime :refer [ParserRuleContext]]
-            [parcera.antlr.clojureLexer :as clojureLexer]
-            [parcera.antlr.clojureParser :as clojureParser]))
+    #_[antlr4.index :as runtime]
+    #_[parcera.antlr.clojureLexer :as clojureLexer]
+    #_[parcera.antlr.clojureParser :as clojureParser]))
 
 
 (set! *warn-on-infer* true)
 
 
-(extend-type ParserRuleContext
-  antlr/ParserRule
-  (children [^ParserRuleContext this] (.-children this))
-  (rule-index [^ParserRuleContext this] (.getRuleIndex this))
-  (start [^ParserRuleContext this] (.getStart this))
-  (end [^ParserRuleContext this] (.getStop this)))
+#_(extend-type ParserRuleContext
+    antlr/ParserRule
+    (children [^ParserRuleContext this] (.-children this))
+    (rule-index [^ParserRuleContext this] (.getRuleIndex this))
+    (start [^ParserRuleContext this] (.getStart this))
+    (end [^ParserRuleContext this] (.getStop this)))
 
 
-(extend-type ErrorNodeImpl
-  antlr/ErrorNode
-  (token [^ErrorNodeImpl this] (.-symbol this)))
+#_(extend-type ErrorNodeImpl
+    antlr/ErrorNode
+    (token [^ErrorNodeImpl this] (.-symbol this)))
 
 
-(extend-type Token
-  antlr/Token
-  (row [^Token this] (.getLine this))
-  (column [^Token this] (.getCharPositionInLine this)))
+#_(extend-type Token
+    antlr/Token
+    (row [^Token this] (.getLine this))
+    (column [^Token this] (.getCharPositionInLine this)))
 
 
-(extend-type clojureParser
-  antlr/AntlrParser
-  (rules [^clojureParser this] (vec (.getRuleNames this)))
-  (tree [^clojureParser this] (. this (code))))
+#_(extend-type clojureParser
+    antlr/AntlrParser
+    (rules [^clojureParser this] (vec (.getRuleNames this)))
+    (tree [^clojureParser this] (. this (code))))
 
 
 (defn parser
-  [input listener]
-  (let [chars  (CharStreams/fromString input)
-        lexer  (doto (new clojureLexer chars)
-                 (.removeErrorListeners))
-        ;; todo: how to handle lexer errors ?
-        ;(.addErrorListener listener))
-        tokens (new CommonTokenStream lexer)]
-    (doto (new clojureParser tokens)
-      (.setBuildParseTree true)
-      (.removeErrorListeners)
-      (.addErrorListener listener))))
+  [input]
+  {:parser input})
+
+#_(defn parser
+    [input listener]
+    (let [chars  (CharStreams/fromString input)
+          lexer  (doto (new clojureLexer chars)
+                   (.removeErrorListeners))
+          ;; todo: how to handle lexer errors ?
+          ;(.addErrorListener listener))
+          tokens (new CommonTokenStream lexer)]
+      (doto (new clojureParser tokens)
+        (.setBuildParseTree true)
+        (.removeErrorListeners)
+        (.addErrorListener listener))))
