6582dc47d3b7cf74b4f65646bffa2f08f75e0db5
Camilo Roca <camilo.roca@cobi.bike>
2019-11-18 19:17:07 +0100

    made clojure a fix dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url  "https://github.com/carocad/parcera/blob/master/LICENSE.md"}
   :source-paths ["src/clojure" "src/javascript"]
   :java-source-paths ["src/java"]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev      {:dependencies   [[criterium/criterium "0.4.5"] ;; benchmark
                                          [org.clojure/test.check "0.10.0"]] ;; generative testing
                         :plugins        [[jonase/eastwood "0.3.5"]] ;; linter
@@ -12,8 +13,7 @@
                         :clean-targets  ^{:protect false} ["target"]}
              ;; java reloader
              ;[lein-virgil "0.1.9"]]
-             :provided {:dependencies [[org.clojure/clojure "1.10.1"]
-                                       [org.antlr/antlr4-runtime "4.7.1"]]}}
+             :provided {:dependencies [[org.antlr/antlr4-runtime "4.7.1"]]}}
 
   :test-selectors {:default     (fn [m] (not (some #{:benchmark} (keys m))))
                    :benchmark   :benchmark}
