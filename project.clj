8fd907e93a06f4095292b0dcfa7e75a2320ae903
Camilo Roca <camilo.roca@cobi.bike>
2019-12-04 21:32:13 +0100

    version bumped [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carocad/parcera "0.5.2"
+(defproject carocad/parcera "0.5.3"
   :description "Grammar-based Clojure parser"
   :url "https://github.com/carocad/parcera"
   :license {:name "LGPLv3"
