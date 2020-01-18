f6e779ec9f087953e0d0e725c1806d200e544be3
Camilo Roca <camilo.roca@cobi.bike>
2020-01-18 22:30:19 +0100

    error listener added to javascript implementation

--- src/clojure/parcera/antlr/javascript.cljs
+++ src/clojure/parcera/antlr/javascript.cljs
@@ -8,6 +8,43 @@
 ; todo: enable this once I know how to make it work properly
 #_(set! *warn-on-infer* true)
 
+;; See parcera.antlr.java for a proper explanation of this
+;; HACK: I dont know of an elegant way to get the same result as I have a pass an object
+;; to antlr with the methods below
+(defn- AntlrFailure
+  [reports]
+  #js {:reportAmbiguity
+                (fn [recognizer dfa start-index stop-index exact ambig-alts configs]
+                  (println "report ambiguity: " recognizer dfa start-index stop-index exact ambig-alts configs))
+
+       :reportAttemptingFullContext
+                (fn [recognizer dfa start-index stop-index conflicting-alts configs]
+                  (println "report attempting full context: " recognizer dfa start-index stop-index conflicting-alts configs))
+
+       :reportContextSensitivity
+                (fn [recognizer dfa start-index stop-index prediction configs]
+                  (println "report context sensitivity: " recognizer dfa start-index stop-index prediction configs))
+
+       :syntaxError
+                (fn [recognizer offending-symbol line column message error]
+                  ;; recognizer is either clojureParser or clojureLexer
+                  (let [report (merge {:row     line
+                                       :column  column
+                                       :message message
+                                       ;; buildParseTrees is true/false; regardless if it is there recognizer is a parser
+                                       :type    (if (some? (.-buildParseTrees recognizer)) :parser :lexer)}
+                                      (when (some? (.-buildParseTrees recognizer))
+                                        {:symbol (str offending-symbol)
+                                         :stack  (->> (.getRuleInvocationStack recognizer)
+                                                      (reverse)
+                                                      (map keyword))})
+                                      (when (some? error)
+                                        {:error error}))]
+                    (vswap! reports conj report)))
+
+       :reports reports})
+
+
 (defn- parser-rule-meta
   [^ParserRuleContext this]
   (let [start ^Token (.-start this)
@@ -57,14 +94,16 @@
 
 (defn parse
   [input]
-  (let [chars  (reader/charStreams input)
-        lexer  (doto (reader/lexer chars)
-                 ;(.removeErrorListeners)
-                 #_(.addErrorListener listener))            ;todo
-        tokens (reader/tokens lexer)
-        parser (reader/parser tokens)]
-    ;(.removeErrorListeners parser) todo
+  (let [listener (AntlrFailure (volatile! ()))
+        chars    (reader/charStreams input)
+        lexer    (doto (reader/lexer chars)
+                   (.removeErrorListeners)
+                   (.addErrorListener listener))
+        tokens   (reader/tokens lexer)
+        parser   (doto (reader/parser tokens)
+                   (.removeErrorListeners)
+                   (.addErrorListener listener))]
     (set! (.-buildParseTrees parser) true)
-    {:rules (into [] (map keyword) (.-ruleNames parser))
-     :tree  (.code parser)}))
-;:reports @(:reports listener)})) todo
+    {:rules   (into [] (map keyword) (.-ruleNames parser))
+     :tree    (.code parser)
+     :reports @(.-reports listener)}))
