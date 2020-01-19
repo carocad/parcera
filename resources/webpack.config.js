2df474b6e1124d84e79cbfbd824329fa657296bd
Camilo Roca <camilo.roca@cobi.bike>
2020-01-19 15:16:49 +0100

    cosmetics

--- resources/webpack.config.js
+++ resources/webpack.config.js
@@ -1,15 +1,3 @@
-/**
- * NOTE: I feel this more as a hack than as an actual solution.
- *
- * Ideally I should just let ClojureScript import the lexer and parser
- * from a browser perspective. However it seems that the Closure compiler
- * doesnt like conditional imports so it throws saying that 'fs' is not
- * defined which is clearly a node.js dependency.
- *
- * On the other hand, if I pass the module as node.js target then the Closure
- * compiler corrupts the source code which makes it unusable :(
- */
-
 const path = require('path')
 
 module.exports = {
