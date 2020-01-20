45a8c573ffbd8108c1a3d97ef829919130fceaa6
Camilo Roca <camilo.roca@cobi.bike>
2020-01-20 16:01:44 +0100

    fix: yet another wrong namespace

--- test/parcera/test_runner.cljs
+++ test/parcera/test_runner.cljs
@@ -1,9 +1,9 @@
 (ns parcera.test-runner
   (:require [figwheel.main.testing :refer-macros [run-tests]]
     ;; require all the namespaces that have tests in them
-            [parcera.test.core :as ct]))
+            [parcera.test-cases :as ct]))
 
 (defn -main []
-  (run-tests 'parcera.test.core))
+  (run-tests 'parcera.test-cases))
 
 ;(set! *main-cli-fn* -main)
