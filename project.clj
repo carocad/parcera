4fba373c43a29153cff83adac387eb6d447eb8fd
Camilo Roca <camilo.roca@cobi.bike>
2020-01-20 15:56:43 +0100

    fix: wrong filepath

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carocad/parcera "0.11.0-SNAPSHOT-2"
+(defproject carocad/parcera "0.11.0-SNAPSHOT-3"
   :description "Grammar-based Clojure(script) parser"
   :url "https://github.com/carocad/parcera"
   :license {:name "LGPLv3"
